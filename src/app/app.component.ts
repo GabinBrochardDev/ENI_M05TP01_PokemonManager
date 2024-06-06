3e125206adb5694570d770d7aad0e8797e1c96ce
gbrochard.dev <gbrochard.dev@gmail.com>
2024-06-06 14:25:29 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GenerationsComponent } from './composants/generations/generations.component';
 
 @Component({
   selector: 'app-root',
